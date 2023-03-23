d10ff574ad1eeaa43350062e7a295b4569fc74c4
auto66 <102926065+auto66@users.noreply.github.com>
2023-03-23 16:05:40 +0100

    Create ranking.js

---
+++ ranking.js
@@ -0,0 +1,70 @@
+const populateTable = (tableName, tableData) => {
+  const table = document.getElementById(tableName);
+  const tableHead = ['Rank', 'Team', 'Count', 'Wins', 'Shootouts', 'Rounds', 'Loss'];
+
+  table.innerHTML = `
+    <thead>
+      <tr>
+        ${tableHead.map(head => `<th>${head}</th>`).join('')}
+      </tr>
+    </thead>
+    <tbody>
+      ${tableData ? tableData.map(rowData => `
+        <tr>
+          ${rowData.map(cellData => `<td>${cellData}</td>`).join('')}
+        </tr>
+      `).join('') : ''}
+    </tbody>
+  `;
+};
+
+const sortTable = (data) => {
+  if(!data) return [];
+
+  const resp = {};
+  data.forEach(entry => {
+    const [teamA, teamB, winner, shootdown, rounds, loss] = entry;
+
+    [teamA, teamB].forEach(team => {
+      if(!resp[team]) resp[team] = {
+        name: team,
+        battles: 0,
+        wins: 0,
+        shootdowns: 0,
+        rounds: 0,
+        loss: 0
+      };
+      resp[team].battles++;
+    });
+
+    resp[winner].wins++;
+    if(shootdown) resp[winner].shootdowns++;
+    resp[winner].rounds += parseInt(rounds);
+    resp[winner].loss += parseInt(loss);
+  });
+
+  const sorted = Object.values(resp).sort((a, b) => {
+    return b.wins - a.wins || b.shootdowns - a.shootdowns || b.rounds - a.rounds || a.loss - b.loss;
+  });
+
+  return sorted.map((item, index) => [
+    index + 1,
+    item.name,
+    item.battles,
+    item.wins,
+    item.shootdowns,
+    item.rounds,
+    `${item.loss}%`
+  ]);
+};
+
+fetch('results.json')
+  .then(response => response.json())
+  .then(data => {
+    const ids = ['A', 'B', 'C', 'D', '2A', '2B'];
+    for (const id of ids) {
+      const name = 'Group_' + id;
+      populateTable(name, sortTable(data[name]));
+    }
+  })
+  .catch(error => console.error(error));
